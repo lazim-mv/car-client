f6949222a12f7b7aff763597e28a1e511c6a3cbb
lazim-mv <lazimlatheef@gmail.com>
2025-03-26 09:27:26 +0530

    improvements filters animations typography contact page

--- src/app/utils/SmoothScrolling.js
+++ src/app/utils/SmoothScrolling.js
@@ -1,22 +1,32 @@
 "use client";
-import { ReactLenis } from "@studio-freight/react-lenis";
+import { useEffect } from "react";
+import Lenis from "lenis";
+
 
 function SmoothScrolling({ children }) {
-  return (
-    <ReactLenis
-      root
-      options={{
-        lerp: 0.1,
-        duration: 1.5,
-        smoothTouch: true,
-        smoothWheel: true,
-        syncTouchLerp: 0.0075,
-        wheelMultiplier:1.3,
-      }}
-    >
-      {children}
-    </ReactLenis>
-  );
-}
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      duration: 1.5,
+      smoothTouch: true,
+      smoothWheel: true,
+      syncTouchLerp: 0.0075,
+      wheelMultiplier: 1.3,
+    });
+
+    const updateScroll = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(updateScroll);
+    };
+
+    requestAnimationFrame(updateScroll);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+};
 
 export default SmoothScrolling;
